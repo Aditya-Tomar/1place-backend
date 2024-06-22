44327bf8a36062cb2c2240208f80dcab891c25bc
Aditya-Tomar <adityatomar55@gmail.com>
2024-06-22 17:34:54 +0530

    removed

--- config/default.js
+++ config/default.js
@@ -1,6 +1,6 @@
 module.exports = {
     MongoDb: {
-        uri: 'mongodb+srv://informaladi:8vvTfrWzJXGkuJyr@cluster0.dxs6lcs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+        uri: '',
         dbName: 'sample_mflix',
         links_collection: 'links',
         users_collection: 'users'
